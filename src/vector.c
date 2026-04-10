26f84b5897eac8c0bc7987b044dc326a92cccbf9
Danil <>
2026-04-10 16:25:45 +0300

    Добавлены новые функции: 1) pop 2) insert 3) get data by index 4) get len, size, capacity

--- src/vector.c
+++ src/vector.c
@@ -6,27 +6,29 @@
 #define VECTOR_GROW_MULT 2
 
 typedef enum {
-  VECTOR_SUCCESS,              // Успешное выполнение
-  VECTOR_EQUALS_NULL,          // Проверка на нулевой вектор не пройдена
-  VECTOR_MALLOC_ERROR,         // Ошибка при аллокации памяти
-  VECTOR_REALLOC_ERROR,        // Ошибка при аллокации памяти
-  VECTOR_ELEM_SIZE_EQUALS_ZERO // Размер элемента вектора равен нулю
+  VECTOR_SUCCESS,        // Успешное выполнение
+  VECTOR_EQUALS_NULL,    // Проверка на нулевой вектор не пройдена
+  VECTOR_MALLOC_ERROR,   // Ошибка при аллокации памяти
+  VECTOR_REALLOC_ERROR,  // Ошибка при аллокации памяти
+  VECTOR_ZERO_ELEM_SIZE, // Размер элемента вектора равен нулю
+  VECTOR_ZERO_LEN,       // Длина вектора равна нулю
+  VECTOR_OUT_OF_BOUND,   // Искомый индекс вне индексов вектора
 } vector_errors_t;
 
 typedef struct {
-  void *data;       // Указатель на область памяти, содержащую данные
-  size_t len;       // Длина вектора
-  size_t capacity;  // Максимальная длина вектора
-  size_t elem_size; // размер одного элемента вектора
+  void *data;    // Указатель на область памяти, содержащую данные
+  int len;       // Длина вектора
+  int capacity;  // Максимальная длина вектора
+  int elem_size; // размер одного элемента вектора
 } vector;
 
 // Выделяет память, инициализирует поля структуры вектора
-vector_errors_t make_vector(vector *v, size_t capacity, size_t elem_size) {
+vector_errors_t make_vector(vector *v, int capacity, int elem_size) {
   if (!v)
     return VECTOR_EQUALS_NULL;
 
   if (!elem_size)
-    return VECTOR_ELEM_SIZE_EQUALS_ZERO;
+    return VECTOR_ZERO_ELEM_SIZE;
 
   if (capacity) {
     v->data = malloc(capacity * elem_size);
@@ -44,7 +46,7 @@ vector_errors_t make_vector(vector *v, size_t capacity, size_t elem_size) {
 }
 
 // Освобождает память массива данных и вектора
-vector_errors_t free_vector(vector *v) {
+vector_errors_t vector_free(vector *v) {
   if (!v)
     return VECTOR_EQUALS_NULL;
 
@@ -58,13 +60,9 @@ vector_errors_t free_vector(vector *v) {
   return VECTOR_SUCCESS;
 }
 
-// Запись нового элемента в конец вектора
-vector_errors_t vector_push_back(vector *v, const void *elem) {
-  if (!v || !elem)
-    return VECTOR_EQUALS_NULL;
-
+vector_errors_t check_capacity(vector *v) {
   if (v->len == v->capacity) {
-    size_t new_capacity =
+    int new_capacity =
         (v->capacity == 0) ? 1 : (v->capacity * VECTOR_GROW_MULT);
 
     void *new_data = realloc(v->data, new_capacity * v->elem_size);
@@ -75,10 +73,87 @@ vector_errors_t vector_push_back(vector *v, const void *elem) {
     v->capacity = new_capacity;
   }
 
-  char *dest = (char *)v->data + v->len * v->elem_size;
+  return VECTOR_SUCCESS;
+}
+
+// Запись нового элемента в конец вектора
+vector_errors_t vector_push_back(vector *v, const void *elem) {
+  if (!v || !elem)
+    return VECTOR_EQUALS_NULL;
+
+  check_capacity(v);
+
+  void *dest = (char *)v->data + v->len * v->elem_size;
   memcpy(dest, elem, v->elem_size);
 
   v->len++;
+
+  return VECTOR_SUCCESS;
+}
+
+vector_errors_t vector_insert(vector *v, const void *elem, int index) {
+  if (!v || !elem)
+    return VECTOR_EQUALS_NULL;
+  if (index < 0 || index >= v->len)
+    return VECTOR_OUT_OF_BOUND;
+
+  check_capacity(v);
+
+  void *index_pointer = (char *)v->data + v->elem_size * index;
+  void *move_dest = (char *)index_pointer + v->elem_size;
+  memmove(move_dest, index_pointer, (v->len - index) * v->elem_size);
+  memcpy(index_pointer, elem, v->elem_size);
+
+  v->len++;
+
+  return VECTOR_SUCCESS;
+}
+
+vector_errors_t vector_pop_back(vector *v) {
+  if (!v)
+    return VECTOR_EQUALS_NULL;
+  if (v->len == 0)
+    return VECTOR_ZERO_LEN;
+
+  v->len--;
+
+  return VECTOR_SUCCESS;
+}
+
+vector_errors_t vector_get(vector *v, void *out, int index) {
+  if (!v)
+    return VECTOR_EQUALS_NULL;
+  if (index < 0 || index >= v->len)
+    return VECTOR_OUT_OF_BOUND;
+
+  memcpy(out, (char *)v->data + index * v->elem_size, v->elem_size);
+
+  return VECTOR_SUCCESS;
+}
+
+vector_errors_t vector_get_len(vector *v, int *out) {
+  if (!v)
+    return VECTOR_EQUALS_NULL;
+
+  *out = v->len;
+
+  return VECTOR_SUCCESS;
+}
+
+vector_errors_t vector_get_cap(vector *v, int *out) {
+  if (!v)
+    return VECTOR_EQUALS_NULL;
+
+  *out = v->capacity;
+
+  return VECTOR_SUCCESS;
+}
+
+vector_errors_t vector_get_elem_size(vector *v, int *out) {
+  if (!v)
+    return VECTOR_EQUALS_NULL;
+
+  *out = v->elem_size;
 
   return VECTOR_SUCCESS;
 }
@@ -88,9 +163,34 @@ int main() {
   make_vector(&v1, 10, sizeof(int));
 
   vector_push_back(&v1, &(int){10});
-  for (int i = 0; i < 10; i++) {
+  vector_push_back(&v1, &(int){20});
+  vector_push_back(&v1, &(int){30});
+  vector_push_back(&v1, &(int){40});
+  vector_push_back(&v1, &(int){50});
+  vector_push_back(&v1, &(int){60});
+  vector_pop_back(&v1);
+
+  int len, cap, size;
+  vector_get_len(&v1, &len);
+  vector_get_cap(&v1, &cap);
+  vector_get_elem_size(&v1, &size);
+
+  printf("Len: %d\nCap: %d\nElem size: %d\n", len, cap, size);
+  for (int i = 0; i < len; i++) {
     printf("%d) %d\n", i, (((int *)(v1.data))[i]));
   }
 
-  free_vector(&v1);
+  int out1, out2, out3;
+  vector_get(&v1, &out1, 2);
+  vector_get(&v1, &out2, 3);
+  vector_get(&v1, &out3, 4);
+  printf("2 elem is %d\n", out1);
+  printf("3 elem is %d\n", out2);
+  printf("4 elem is %d\n", out3);
+
+  vector_insert(&v1, &(int){42}, 2);
+  for (int i = 0; vector_get_len(&v1, &len), i < len; i++) {
+    printf("%d) %d\n", i, (((int *)(v1.data))[i]));
+  }
+  vector_free(&v1);
 }
